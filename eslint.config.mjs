601139eecedb9c95060cceb2193204a836189c33
Bastien Caudan <bastien.caudan@datadoghq.com>
2026-02-04 15:51:17 +0100

    ⬆️ add and configure `unicorn/prefer-node-protocol`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import prettier from 'eslint-plugin-prettier/recommended';
+import unicorn from 'eslint-plugin-unicorn';
 
 export default tseslint.config(
   {
@@ -15,12 +16,16 @@ export default tseslint.config(
         tsconfigRootDir: import.meta.dirname,
       },
     },
+    plugins: {
+      unicorn,
+    },
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unsafe-assignment': 'off',
       '@typescript-eslint/no-unsafe-argument': 'off',
       '@typescript-eslint/no-unsafe-call': 'off',
       '@typescript-eslint/no-unsafe-member-access': 'off',
+      'unicorn/prefer-node-protocol': 'error',
     },
   },
   prettier
