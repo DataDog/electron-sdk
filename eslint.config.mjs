6cf3cb72a2832d2cc70d6a40056f032830826a49
Bastien Caudan <bastien.caudan@datadoghq.com>
2026-02-10 10:30:22 +0100

    🐛 make lint fail

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,7 +20,6 @@ export default tseslint.config(
       unicorn,
     },
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unsafe-assignment': 'off',
       '@typescript-eslint/no-unsafe-argument': 'off',
       '@typescript-eslint/no-unsafe-call': 'off',
